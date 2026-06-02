5049f09559d2aa5b87f1431f82129df960660f83
katherine lopez <codedbykatherine@gmail.com>
2026-06-02 13:00:26 -0400

    16, tokyo specific query

--- 14_salary.sql
+++ 14_salary.sql
@@ -1,3 +1,8 @@
 select *
 from Employees
-where Salary > 200 and Salary < 2500
+where Salary > 2000 and Salary < 2500
+
+# or u can write it
+#select *
+#from Employees
+#where salary between 2000 and 2500;
---
+++ 16_product_table.sql
@@ -0,0 +1,5 @@
+select SupplierID,ProductName
+from Products;
+select SupplierID,ProductName
+from Products
+where SupplierID = 4
